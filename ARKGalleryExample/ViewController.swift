d350c3d5b1a06426ee1e8dfa233f0b191678884c
Aswin Raj Kharel <query.ark@gmail.com>
2018-12-28 10:00:41 -0600

    ARKGalleryExample Project Added

---
+++ ARKGalleryExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ARKGalleryExample
+//
+//  Created by Aswin Raj Kharel on 12/28/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
