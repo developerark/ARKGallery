995a7a3af349fa795f0562b245160f46388c32ea
Aswin Raj Kharel <query.ark@gmail.com>
2018-12-28 09:55:37 -0600

    Project Setup

---
+++ ARKGallery/ARKGallery.h
@@ -0,0 +1,18 @@
+//
+//  ARKGallery.h
+//  ARKGallery
+//
+//  Created by Aswin Raj Kharel on 12/28/18.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ARKGallery.
+FOUNDATION_EXPORT double ARKGalleryVersionNumber;
+
+//! Project version string for ARKGallery.
+FOUNDATION_EXPORT const unsigned char ARKGalleryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ARKGallery/PublicHeader.h>
+
+
